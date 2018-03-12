f1656bbdf3eb1c5ef521aaff9ac0d08601360035
Hackbright Student <no-reply@hackbrightacademy.com>
2018-03-12 13:07:01 -0700

    sort output by word count

--- wordcount.py
+++ wordcount.py
@@ -1,20 +1,58 @@
+from sys import argv
+from collections import Counter
+from string import punctuation
+
+if len(argv) < 2:
+    input_file = "test.txt"
+else:
+    input_file = argv[1]
 # put your code here.
+
+
+# def print_word_count(filename):
+#     """ Prints the count of each word in the file. """
+
+#     word_dict = {}
+#     punctuation = "!@#$%^&*()_+=-,./?><\\|{}][;:'\""
+
+#     textfile = open(filename)
+
+#     for line in textfile:
+#         line = line.strip()
+#         words = line.split(" ")
+#         for word in words:
+#             word = word.strip(punctuation).lower()
+#             word_dict[word] = word_dict.get(word, 0) + 1
+
+#     textfile.close()
+
+#     for word, count in word_dict.items():
+#         print word, count
+
+#     print len(word_dict)
+
+
 def print_word_count(filename):
     """ Prints the count of each word in the file. """
 
-    word_dict = {}
-
     textfile = open(filename)
 
-    for line in textfile:
-        line = line.strip()
-        words = line.split(" ")
-        for word in words:
-            word_dict[word] = word_dict.get(word, 0) + 1
+    file_string = textfile.read()
+    words = file_string.split()
+
+    words = [word.strip(punctuation).lower() for word in words]
+    word_count = Counter(words)
 
     textfile.close()
 
-    for word, count in word_dict.items():
+    # alpha_list = sorted(word_count.keys())
+
+    # for word in alpha_list:
+    #     print word, word_count[word]
+
+    item_list = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
+
+    for word, count in item_list:
         print word, count
 
-print_word_count('twain.txt')
+print_word_count(input_file)
