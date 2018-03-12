cb14a71bb713ec91792f83b9d2dcca53feb1e362
Hackbright Student <no-reply@hackbrightacademy.com>
2018-03-12 11:59:17 -0700

    completed print_word_count function

--- wordcount.py
+++ wordcount.py
@@ -1,1 +1,20 @@
 # put your code here.
+def print_word_count(filename):
+    """ Prints the count of each word in the file. """
+
+    word_dict = {}
+
+    textfile = open(filename)
+
+    for line in textfile:
+        line = line.strip()
+        words = line.split(" ")
+        for word in words:
+            word_dict[word] = word_dict.get(word, 0) + 1
+
+    textfile.close()
+
+    for word, count in word_dict.items():
+        print word, count
+
+print_word_count('twain.txt')
